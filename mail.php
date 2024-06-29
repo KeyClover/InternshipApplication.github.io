6bf81bdb2e39913ec70463ec73cb9d3843dad69f
KeyClover <102015852+KeyClover@users.noreply.github.com>
2024-06-30 00:42:02 +0700

    Add files via upload

--- mail.php
+++ mail.php
@@ -5,15 +5,16 @@ $first_name = $_POST['First Name'];
 $last_name = $_POST['Last Name'];
 $email = $_POST['email'];
 $phone_no = $_POST['phone_no'];
-$myemail= $_POST["SuvichakForWork45@outlook.com"]
-$mailheader = "From: HR department auto email reply.""<" .$myemail. ">\r\n";
-
+$headers = "From:" "<" 'SuvichakForWork45@outlook.com' ">\r\n" .
 $recipient = $email;
 
 
 
-mail($recipient,"We have received your application.","Thank you for your application. We have received it and our staff will respond soon. Please relax and enjoy your day.",$mailheader)
+mail($recipient,"We have received your application.","Thank you for your application. We have received it and our staff will respond soon. Please relax and enjoy your day.",$headers)
 or die("Error!");
 
 echo"message send";
 ?>
+
+
+
