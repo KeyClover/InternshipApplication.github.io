f2c21cf2e1571c901898f23114caf8aaeab5ed23
KeyClover <102015852+KeyClover@users.noreply.github.com>
2024-06-30 00:05:45 +0700

    Add files via upload

--- mail.php
+++ mail.php
@@ -5,9 +5,12 @@ $first_name = $_POST['First Name'];
 $last_name = $_POST['Last Name'];
 $email = $_POST['email'];
 $phone_no = $_POST['phone_no'];
+$myemail= $_POST["SuvichakForWork45@outlook.com"]
+$mailheader = "From: HR department auto email reply.""<" .$myemail. ">\r\n";
 
-$mailheader = "From: HR department auto email reply.\r\n";
 $recipient = $email;
+
+
 
 mail($recipient,"We have received your application.","Thank you for your application. We have received it and our staff will respond soon. Please relax and enjoy your day.",$mailheader)
 or die("Error!");
