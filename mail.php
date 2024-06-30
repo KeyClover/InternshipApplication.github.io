f9f3d9f7ae34b668bd1cfadde2f946b6a6e7cb8c
KeyClover <102015852+KeyClover@users.noreply.github.com>
2024-06-30 14:33:45 +0700

    Add files via upload

--- mail.php
+++ mail.php
@@ -6,39 +6,33 @@ use PHPMailer\PHPMailer\Exception;
 
 
 require "vendor/autoload.php";
+require 'vendor/phpmailer/phpmailer/scr/Exception.php';
+require 'vendor/phpmailer/phpmailer/scr/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/scr/SMTP.php';
 
+
+
+if(isset($_POST["send"])){
 $mail = new PHPMailer(true);
-
-$first_name = $_POST["First Name"];
-$last_name = $_POST["Last Name"];
-$phone_no = $_POST["phone_no"];
-
-
-
-try {
 $mail ->isSMTP();
 $mail ->SMTPAuth = true;
 
 $mail ->Host = "smtp.gmail.com";
-$mail ->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail ->SMTPSecure = 'ssl';
 $mail ->Port = 465;
 
 $mail ->Username ="suvichakkey45@gmail.com";
 $mail ->Password ="nrjb qjqe drjc zfbj";
 
-$mail ->setFrom('suvichakkey45@gmail.com', 'Auto Reply');
+$mail ->setFrom("suvichakkey45@gmail.com");
 
 $mail->addAddress($_POST["email"]);
 
-$mail->Body = "We have received your application.","Thank you for your application. We have received it and our staff will respond soon. Please relax and enjoy your day";
+$mail->isHTML(true);
 
-$mail->send();
+$mail->Body = 'We have received your application.","Thank you for your application. We have received it and our staff will respond soon. Please relax and enjoy your day';
 
-echo 'We have received your application';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-?>
+$mail->send(); }
 
 
 
